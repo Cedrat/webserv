53b593d4feaf53a63729fdecc413535dc9cfd2c2
Cedrat <lnoaille@student.fr>
2021-06-23 22:05:39 +0200

    :hammer: Add Somes utils

---
+++ SocketClient.cpp
@@ -0,0 +1,40 @@
+#include "SocketClient.hpp"
+
+SocketClient::SocketClient(void) : _nb_sockets(0)
+{
+
+}
+
+
+SocketClient::~SocketClient()
+{
+
+}
+
+void SocketClient::addSocketClient(fd sockfd)
+{
+	_array_of_socket.push_back(sockfd);
+	_nb_sockets++;
+	std::cout << _nb_sockets << std::endl;
+}
+
+void SocketClient::closeAndRemoveSocket(size_t idx)
+{
+	close(_array_of_socket[idx]);
+	
+	std::vector<fd>::iterator it_start;
+	it_start = _array_of_socket.begin();
+	it_start += idx;
+	_array_of_socket.erase(it_start);
+	_nb_sockets--;
+}
+
+fd & SocketClient::operator[](size_t idx)
+{
+	return (_array_of_socket[idx]);
+}
+
+const fd& SocketClient::operator[](size_t idx) const
+{
+	return (_array_of_socket[idx]);
+}
---
+++ SocketClient.hpp
@@ -0,0 +1,25 @@
+#ifndef SOCKETCLIENT_HPP
+# define SOCKETCLIENT_HPP
+
+#include <string>
+#include "fonction.hpp"
+#include <vector>
+
+class SocketClient
+{
+	public : 
+		SocketClient(void);
+		~SocketClient();
+		void addSocketClient(fd sockfd);
+
+		fd 		&operator[](size_t idx);
+		const	fd &operator[](size_t idx) const;
+		void	closeAndRemoveSocket(size_t idx);
+
+
+	private : 
+		size_t	_nb_sockets;
+		std::vector<fd> _array_of_socket;
+};
+
+#endif
