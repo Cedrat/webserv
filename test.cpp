bd85c359550da6fcd35f78f19e227bc1dadeebf8
Cedrat <lnoaille@student.fr>
2021-09-29 13:12:51 +0200

    Little modification on conf

--- test.cpp
+++ test.cpp
@@ -4,23 +4,23 @@
 int main()
 {
     std::string *_deux;
-    _deux = nullptr;
+    _deux = NULL;
     if (_deux)
         std::cout << "Coucou " << std::endl;
-    if (_deux == nullptr)
+    if (_deux == NULL)
         std::cout << "Absent" << std::endl;
 
     _deux = new std::string("un");
     if (_deux)
         std::cout << "Coucou " << std::endl;
-    if (_deux == nullptr)
+    if (_deux == NULL)
         std::cout << "Absent" << std::endl;
     
     delete _deux;
-    _deux = nullptr;
+    _deux = NULL;
 
     if (_deux)
         std::cout << "Coucou " << std::endl;
-    if (_deux == nullptr)
+    if (_deux == NULL)
         std::cout << "Absent" << std::endl;
 }
